15d98ab42adfd9f299387e39354063140c464e36
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2024-10-05 00:02:03 -0600

    install ui components library

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,17 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { useColorScheme } from "react-native";
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+
+
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ApplicationProvider  {...eva} theme={colorScheme === 'dark' ? eva.dark : eva.light}>
+      <IconRegistry icons={EvaIconsPack} />
+
       <Stack
         screenOptions={{
           headerShown: false,
@@ -17,6 +19,7 @@ export default function RootLayout() {
       >
         <Stack.Screen name="index" />
       </Stack>
-    </ThemeProvider>
+    </ApplicationProvider>
+
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
+
+import { Button, Icon, Layout, Text } from '@ui-kitten/components';
+import { useState } from 'react';
 
 export default function Index() {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+
+      <Text category='h1'>HOME</Text>
+      <Text category='p1' style={{marginVertical: 10}}>{counter}</Text>
+      <Button onPress={() => setCounter(counter + 1)} appearance='outline'>
+        +1
+      </Button>
+
+    </Layout>
   );
 }
