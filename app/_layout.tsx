c8a8eb76137b6e983c8fd0eb854359bacfceeebd
Fabiola <fabiola.escobedo4260@alumnos.udg.mx>
2024-10-07 15:22:47 -0600

    add initial screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,27 @@
 import { Stack } from "expo-router";
 import { useColorScheme } from "react-native";
-import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-import * as eva from '@eva-design/eva';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
-
-
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
+import * as eva from "@eva-design/eva";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   return (
-    <ApplicationProvider  {...eva} theme={colorScheme === 'dark' ? eva.dark : eva.light}>
+    <ApplicationProvider
+      {...eva}
+      theme={colorScheme === "dark" ? eva.dark : eva.light}
+    >
       <IconRegistry icons={EvaIconsPack} />
 
       <Stack
         screenOptions={{
           headerShown: false,
         }}
+        initialRouteName="initial"
       >
-        <Stack.Screen name="index" />
+        <Stack.Screen name="initial" />
+        {/* <Stack.Screen name="index" /> */}
       </Stack>
     </ApplicationProvider>
-
   );
 }
