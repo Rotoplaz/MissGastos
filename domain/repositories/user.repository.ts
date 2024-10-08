9fabe2642ba32d9a04c4ec25e19c5c04918afd8f
Carlos Flores <perezflorescarlosantonio@gmail.com>
2024-10-08 12:24:21 -0600

    Added file migration.ts on db, and modified file user-sqli.repository.impl.ts

--- domain/repositories/user.repository.ts
+++ domain/repositories/user.repository.ts
@@ -3,5 +3,5 @@ import { User } from "../entities/user.entity";
 export interface UserRepository {
   getUser(): Promise<User | null>;
   createUser(user: User): Promise<User>;
-  updateUser(user: Partial<User>): Promise<User | null>;
+  updateUser(user: Partial<User>): Promise<User>;
 }
