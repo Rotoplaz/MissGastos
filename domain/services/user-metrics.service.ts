b9a87ee23cf145fcc863e770bbcc83cd08d84c7d
Sutekibb <leonel.hernandez0355@alumnos.udg.mx>
2024-10-14 21:14:46 -0600

    WIP: User metrics expence

--- domain/services/user-metrics.service.ts
+++ domain/services/user-metrics.service.ts
@@ -1,17 +1,35 @@
 import { ExpenseSqliteRepositoryImpl } from "@/infrastructure/expense/expense-sqlite.repository.impl";
 import { ExpenseRepository } from "../repositories/expense.repository";
+import { UserRepository } from "../repositories/user.repository";
 
 export class UserMetricsService {
     constructor(
-        private readonly expenseRepository:ExpenseRepository
+        private readonly expenseRepository:ExpenseRepository,
+        private readonly userRepository:UserRepository
     ){}
     async getExpenseMetrics(){
-        const allExpenseGroupByCategory = await this.expenseRepository.getExpensesGroupByCategory()
-        allExpenseGroupByCategory
+        const allExpenseGroupByCategory = await this.expenseRepository.getExpensesGroupByCategory();
+        const totalSum = allExpenseGroupByCategory.reduce((acc, item) => acc + item.totalExpense, 0);
+        const totalExpensePercentages = allExpenseGroupByCategory.map(item =>{
+            return{
+                type:item.type,
+                porcentage: (item.totalExpense / totalSum) * 100
+            };
+        });
 
         return {
-            allExpenseGroupByCategory
+            allExpenseGroupByCategory, totalSum, totalExpensePercentages
         }
     }
+
+    async getExpenseMetricsControl(){
+        const user = await this.userRepository.getUser();
+        const allExpense = await this.expenseRepository.getAllExpenses();
+        const totalSumExpense = allExpense.reduce((acc, item) => acc + item.amount, 0);
+
+
+    }
+
+
 
 }
