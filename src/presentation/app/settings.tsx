18e09c813d284f69c5a5790c69ca7f39f9435091
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2024-10-29 17:01:01 -0600

    datepicker

--- src/presentation/app/settings.tsx
+++ src/presentation/app/settings.tsx
@@ -1,40 +1,68 @@
-import { Button, Icon, Layout, Menu, MenuItem, Text } from '@ui-kitten/components'
-import { router } from 'expo-router'
-import * as SqliteDatabase from 'expo-sqlite'
-import React from 'react'
-import { useUserStore } from '../store/user/useUserStore'
-import { useTheme } from '@react-navigation/native'
-import { LayoutWithTopNavigation } from '../common/layouts/LayoutWithTopNavigation'
+import { Icon, Layout, Menu, MenuItem, Text } from "@ui-kitten/components";
+import { router } from "expo-router";
+import * as SqliteDatabase from "expo-sqlite";
+import React from "react";
+import { useUserStore } from "../store/user/useUserStore";
+import { useTheme } from "@react-navigation/native";
+import { LayoutWithTopNavigation } from "../common/layouts/LayoutWithTopNavigation";
+import { Alert } from "react-native";
 
 export const config = () => {
-  const setUser = useUserStore(state => state.setUser);
   const theme = useTheme();
-
-  return (
-    <LayoutWithTopNavigation TitleScreen="Configuración">
-      <Layout style={{paddingHorizontal: 10, flex: 1, gap: 10,paddingBottom: 15}} >
-
-
-        <Menu style={{ backgroundColor: theme.colors.background}}>
-          <MenuItem title='Usuario' accessoryLeft={<Icon name="person" />} onPress={()=>router.push("/profile")}/>
-
-        </Menu>
-
-
-          <Button onPress={async()=> {
+  const setUser = useUserStore(state=>state.setUser);
+  
+  const handleDeleteDatabaseInformation = async()=> {
+    Alert.alert("Cuidado", "Seguro de eliminar toda tu información",
+      [
+        {
+          text: "Cancelar",
+          style: "cancel",
+        },
+        {
+          text: "Confirmar",
+          style: "destructive",
+          onPress: async() => {
             try {
               const database = await SqliteDatabase.openDatabaseAsync("MissGastosDataBase");
               await database.closeAsync();
               await SqliteDatabase.deleteDatabaseAsync("MissGastosDataBase");
               setUser(null);
-              router.replace("/initial");
+              router.replace("/");
             } catch (error) {
               console.log(error)
             }
-          }} status='danger'>Borrar Base de datos</Button>
+          }, 
+        },
+      ]
+    )
+  }
+
+  return (
+    <LayoutWithTopNavigation TitleScreen="Configuración">
+      <Layout
+        style={{ paddingHorizontal: 10, flex: 1, gap: 10, paddingBottom: 15 }}
+      >
+        <Menu style={{ backgroundColor: theme.colors.background }}>
+          <MenuItem
+            title="Usuario"
+            accessoryLeft={<Icon name="person" />}
+            onPress={() => router.push("/profile")}
+          />
+          <MenuItem
+            
+            style={{justifyContent: "flex-start"}}
+            title={(evaProps) => (
+              <Text status="danger" >
+                Borrar toda mi información
+              </Text>
+            )}
+            accessoryLeft={<Icon fill='red' name="trash-2-outline" />}
+            onPress={handleDeleteDatabaseInformation}
+          />
+        </Menu>
       </Layout>
     </LayoutWithTopNavigation>
-  )
-}
+  );
+};
 
 export default config;
--- src/presentation/createCard/components/CreditCardForm.tsx
+++ src/presentation/createCard/components/CreditCardForm.tsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { Alert, StyleSheet } from "react-native";
-import { Button, Calendar, Input, Layout, Text } from "@ui-kitten/components";
+import { Button, Calendar, Datepicker, Input, Layout, Text } from "@ui-kitten/components";
 import { CreateCreditCardUseCase } from "@/src/application/use-cases/creditCard/create-credit-card.user-case";
 import { CreditCardCrudRepository } from "@/src/infrastructure/cards/credit-card-crud.repository.impl";
 import { useCreditCardsStore } from "../../store/credit-cards/useCreditCardsStore";
@@ -21,9 +21,9 @@ interface FormData {
 const creditCardSchema = z.object({
   name: z.string().min(1,"Nombre es requerido"),
   lastFourDigits: z.string().length(4, "Debe tener 4 dígitos"),
-  debt: z.coerce.number().min(1, "Debe ser un número válido"),
+  debt: z.coerce.number().min(0, "Debe ser un número válido"),
   creditLimit: z.coerce.number().min(1, "Debe ser un número válido"),
-  dueDate: z.coerce.number().min(1).max(31, "Debe ser un día del 1 al 31")
+  dueDate: z.coerce.date()
 });
 
 export const CreditCardForm = () => {
@@ -46,7 +46,7 @@ export const CreditCardForm = () => {
     const newCard = await new CreateCreditCardUseCase(repository).execute({
       creditLimit: +data.creditLimit,
       debt: +data.debt,
-      dueDate: new Date("2029-10-10"),
+      dueDate: new Date(data.dueDate),
       lastFourDigits: data.lastFourDigits,
       name: data.name,
       type: "credit"
@@ -107,15 +107,18 @@ export const CreditCardForm = () => {
 
       <Layout>
         <Text style={style.label}>Fecha límite</Text>
-        <Calendar
-          style={style.calendar}
-          // date={date}
-          onSelect={(e:any)=>console.log(e)}
-        />
+          <Datepicker
+            date={watch("dueDate")}
+            onSelect={nextDate => {
+              setValue("dueDate", nextDate)
+              console.log(nextDate)
+            }}
+          />
+
         {errors.dueDate && <Text style={style.error}>{errors.dueDate.message}</Text>}
       </Layout>
 
-      <Button style={style.submitButton} status="danger" onPress={handleSubmit(onSubmit)}>
+      <Button style={style.submitButton} onPress={handleSubmit(onSubmit)}>
         Enviar
       </Button>
     </>
