7b7fa73e6166f9650a7c543d859aaf5ad9668437
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2024-10-31 20:48:56 -0600

    refactor cretedebit component

--- src/presentation/createCard/components/DebitCardForm.tsx
+++ src/presentation/createCard/components/DebitCardForm.tsx
@@ -1,71 +1,16 @@
 import { Button, Input, Layout, Text } from "@ui-kitten/components";
-import React, { useRef } from "react";
-import { Alert, StyleSheet } from "react-native";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
-import { DebitCard } from "@/src/domain/entities/payment-methods.entity";
-import { DebitCardRepositoryImpl } from "@/src/infrastructure";
-import { CreateDebitCardUseCase } from "@/src/application";
-import { router } from "expo-router";
-import { useCardsStore } from "../../store";
+import React from "react";
+import { StyleSheet } from "react-native";
 
+import { DebitCard } from "@/src/domain/entities/payment-methods.entity";
+import { useDebitCardForm } from "../hooks/useDebitCardForm";
 
 interface Props {
   debitCard: DebitCard | null;
 }
 
-const debitCardSchema = z.object({
-  name: z.string().min(1, "Alias es requerido"),
-  lastFourDigits: z.string().length(4, "Debe tener 4 dígitos"),
-  debt: z.coerce.number().optional().default(0),
-  limit: z.coerce.number().min(1, "Debe ser un número válido"),
-  currentBalance: z.coerce.number().optional().default(0),
-});
-
-interface FormData {
-  name: string;
-  lastFourDigits: string;
-  debt: string;
-  limit: string;
-  currentBalance: string;
-}
-
 export const DebitCardForm = ({ debitCard }:Props) => {
-  const {
-    handleSubmit,
-    setValue,
-    watch,
-    formState: { errors },
-  } = useForm<FormData>({
-    resolver: zodResolver(debitCardSchema),
-    defaultValues: {
-      name: debitCard?.name || "",
-      lastFourDigits:  debitCard?.lastFourDigits || "",
-      debt: debitCard?.debt.toString() || "",
-      limit: debitCard?.limit.toString() || "",
-      currentBalance: debitCard?.currentBalance.toString() || "",
-    },
-  });
-  const debitCardRepository = useRef(new DebitCardRepositoryImpl());
-  const addCardInStore = useCardsStore(state=>state.addCard)
-  const onSubmit = async (data: FormData) => {
-    if(!debitCard) {
-      const newCard = await new CreateDebitCardUseCase(debitCardRepository.current).execute({
-        currentBalance: +data.currentBalance,
-        debt: +data.debt,
-        lastFourDigits: data.lastFourDigits,
-        limit: +data.limit,
-        name: data.name,
-        type: "debit"
-      });
-      addCardInStore(newCard);
-      Alert.alert("Éxito", "Formulario enviado correctamente.");
-      router.back();
-    }
-
-  };
-
+  const { errors, handleSubmit, setValue, watch, onSubmit } = useDebitCardForm(debitCard);
   return (
     <>
       <Layout style={style.inputContainer}>
