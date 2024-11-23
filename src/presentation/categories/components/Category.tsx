bb86e657acf5af32683d98d33ed507c4bb5a1b8e
Fabiola <fabiola.escobedo4260@alumnos.udg.mx>
2024-11-23 00:44:52 -0600

    History screen + categories

--- src/presentation/categories/components/Category.tsx
+++ src/presentation/categories/components/Category.tsx
@@ -1,40 +1,49 @@
-import { Category as CategoryEntity } from '@/src/domain/entities/category.entity'
-import { Button, Icon, Layout, Text } from '@ui-kitten/components';
-import React from 'react'
-import { GestureResponderEvent, StyleProp, StyleSheet, ViewStyle } from 'react-native';
+import { Category as CategoryEntity } from "@/src/domain/entities/category.entity";
+import { Button, Icon, Layout, Text } from "@ui-kitten/components";
+import React from "react";
+import {
+  GestureResponderEvent,
+  StyleProp,
+  StyleSheet,
+  ViewStyle,
+} from "react-native";
 
 interface Props {
   category: CategoryEntity;
   style?: StyleProp<ViewStyle>;
-  onPress?: ((event: GestureResponderEvent) => void) | undefined
+  onPress?: ((event: GestureResponderEvent) => void) | undefined;
 }
 
-export const Category = ({category, style, onPress}:Props) => {
+export const Category = ({ category, style, onPress }: Props) => {
   return (
-    <Layout style={{justifyContent: "center", alignItems: "center", gap: 5}}>
+    <Layout style={{ justifyContent: "center", alignItems: "center", gap: 5 }}>
       <Button
-      appearance="ghost"
-      style={[{
-        backgroundColor: category.color,
-        borderRadius: 100,
-        paddingHorizontal: 12,
-        paddingVertical: 12
-      }, style]}
-      onPress={onPress}
-      accessoryLeft={() => (
+        appearance="ghost"
+        style={[
+          {
+            backgroundColor: category.color,
+            borderRadius: 100,
+            paddingHorizontal: 12,
+            paddingVertical: 12,
+          },
+          style,
+        ]}
+        onPress={onPress}
+        accessoryLeft={() => (
           <Icon name={category.icon} style={[styles.iconCircle]} fill="white" />
-      )}
-    />
-    <Text category="c2" appearance='hint'>{category.type}</Text>
+        )}
+      />
+      <Text category="c2" appearance="hint">
+        {category.type}
+      </Text>
     </Layout>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
-    iconCircle: {
-        width: 30,
-        height: 30,
-        borderRadius: 50
-    },
+  iconCircle: {
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+  },
 });
